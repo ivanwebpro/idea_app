fef78f1badc7d80a788c5237479f42b57bbbb429
Ivan Skorodumov <ivan@ivanpro.com>
2014-12-06 10:30:40 +0000

    DB backup script

---
+++ db/run_backup.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$dt = date("Y-m-d_H-i-s");
+
+$backupname_db_sql= "db_".$dt.".sql";
+
+$MYSQL_HOST = "localhost";
+$MYSQL_USER = "idea_app";
+$MYSQL_DB_NAME = "idea_app";
+$MYSQL_PASSWORD = "674jtg9^u90";
+$MYSQL_CHARSET = "utf8";
+$MYSQL_DEBUG = false;
+$MYSQL_PREFIX = '';
+
+$webroot = "~/Web";
+$backupname_path = "$webroot/db/$backupname_db_sql";
+
+$command = "mysqldump  --opt --skip-extended-insert --lock-tables=false -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DB_NAME > $backupname_path"; 
+
+ob_start();
+//print "<hr>$command<hr>";
+system($command);
+ob_end_clean();
